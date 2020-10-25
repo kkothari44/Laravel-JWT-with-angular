393390c1cf0d4da7f3f7f5795dcb8a6980764369
keshav <keshav.kothari@broctagon.com>
2020-10-26 03:36:55 +0530

    frontend

---
+++ frontend/src/app/_services/authentication.service.ts
@@ -0,0 +1,71 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '@/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    register(name,email,password) {
+        return this.http.post<any>(`${config.apiUrl}/api/auth/signup`, { name, email, password })
+    }
+
+    login(email, password) {
+        let headers = new HttpHeaders();
+        
+        return this.http.post<any>(`${config.apiUrl}/api/auth/login`, { email, password })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', JSON.stringify(user));
+                localStorage.setItem('Authorization',user.Authorization);
+                this.currentUserSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+
+    getLogin(){
+        let authorization = localStorage.getItem("Authorization");
+        const headers= new HttpHeaders()
+                .set('content-type', 'application/json')
+                .set('Authorization',authorization);
+        
+        return this.http.get(`${config.apiUrl}/api/auth/user`, { 'headers': headers });
+    }
+
+    edit(name, password) {
+        let authorization = localStorage.getItem("Authorization");
+        const headers= new HttpHeaders()
+                .set('content-type', 'application/json')
+                .set('Authorization',authorization);
+        return this.http.post<any>(`${config.apiUrl}/api/auth/edit`, { name, password },{headers});
+    }
+
+    delete(id: number) {
+        let authorization = localStorage.getItem("Authorization");
+        const headers= new HttpHeaders()
+                .set('content-type', 'application/json')
+                .set('Authorization',authorization);
+        
+        return this.http.get(`${config.apiUrl}/api/auth/delete`, { 'headers': headers });
+    }
+}
---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,96 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@/_models';
+import { AuthenticationService, AlertService } from '@/_services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    userName;
+    userEmail;
+    userId;
+    loading = false;
+    clicked = false;
+    user:any = [];
+    editForm: FormGroup;
+    isEdit = false;
+    constructor(
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService,
+        private formBuilder: FormBuilder,
+
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.authenticationService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.loadAllUsers());
+    }
+
+    private loadAllUsers() {
+        this.loading = true;
+        this.authenticationService.getLogin().pipe(first())
+            .subscribe(data => {
+                localStorage.setItem('email',data['data'].email);
+                localStorage.setItem('name',data['data'].name);
+                localStorage.setItem('id',data['data'].id);
+            },
+            error => {
+                    this.alertService.error(error);
+                    
+                    this.loading = false;
+            });
+    }
+
+    public showUserData(event){
+        this.userEmail = localStorage.getItem('email');
+        this.userName = localStorage.getItem('name');
+        this.userId = localStorage.getItem('id');
+        this.clicked = true;
+    }
+
+    public editUserDetail(){
+        this.isEdit = true;
+        this.loading = false;
+        this.editForm = this.formBuilder.group({
+            username: [this.userName, Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+    // convenience getter for easy access to form fields
+    get f() { return this.editForm.controls; }
+    onSubmit() {
+        
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.editForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.authenticationService.edit(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.loading = false;
+                    this.isEdit = false;
+                    this.clicked = false;
+                    this.ngOnInit();
+                },
+                error => {
+                    this.alertService.error(error);
+                    
+                    this.loading = false;
+                });
+    }
+}
